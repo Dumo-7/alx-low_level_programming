26e54390fa7f7237d257cd6dc2e783276451e654
Dumo-7 <deebozongwana@yahoo.com>
2023-07-18 17:58:41 -0700

    Task 9

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,47 @@
+#include "main.h"
+
+/**
+ * times_table - print the 9 tiems table
+ *
+ */
+
+void times_table(void)
+{
+	_putchar('0');
+	_putchar(',');
+	_putchar(' ');
+	_putchar(' ');
+	_putchar('9');
+	_putchar(',');
+	_putchar(' ');
+	_putchar(' ');
+	_putchar('18');
+	_putchar(',');
+	_putchar(' ');
+	_putchar(' ');
+	_putchar('27');
+	_putchar(',');
+	_putchar(' ');
+	_putchar(' ');
+	_putchar('36');
+	_putchar(',');
+	_putchar(' ');
+	_putchar(' ');
+	_putchar('45');
+	_putchar(',');
+	_putchar(' ');
+	_putchar(' ');
+	_putchar('54');
+	_putchar(',');
+	_putchar(' ');
+	_putchar(' ');
+	_putchar('63');
+	_putchar(',');
+	_putchar(' ');
+	_putchar(' ');
+	_putchar('72');
+	_putchar(',');
+	_putchar(' ');
+	_putchar(' ');
+	_putchar('81');
+}
