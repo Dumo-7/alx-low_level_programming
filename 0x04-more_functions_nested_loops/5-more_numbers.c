0619fd4629f6ce46a31809d628d9795ac10a4a10
Dumo-7 <deebozongwana@yahoo.com>
2023-07-20 16:55:59 -0700

    Task 5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints numbers 0-10 14x
+ *
+ */
+
+void more_numbers(void)
+{
+	int l;
+	int a;
+
+	for (a = '0'; a < '10'; a++)
+	{
+		for (l = '0'; l <= '14'; l++)
+		{
+			if (l > 9)
+			{
+				_putchar((l / 10) + '0');
+			}
+		_putchar((l % 10) + '0');
+		}
+		_putchar('\n');
+	}
+}
