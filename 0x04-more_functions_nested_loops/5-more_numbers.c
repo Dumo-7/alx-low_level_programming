ca9db7a8f8911486823bd6a6da9a158cbb7e0be7
Dumo-7 <deebozongwana@yahoo.com>
2023-07-21 16:13:51 -0700

    Task Updates

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -10,16 +10,16 @@ void more_numbers(void)
 	int l;
 	int a;
 
-	for (a = '0'; a < '10'; a++)
+	for (a = 0; a < 10; a++)
 	{
-		for (l = '0'; l <= '14'; l++)
+		for (l = 0; l <= 14; l++)
 		{
 			if (l > 9)
 			{
 				_putchar((l / 10) + '0');
 			}
-		_putchar((l % 10) + '0');
+			_putchar((l % 10) + '0');
 		}
-		_putchar('\n');
+			_putchar('\n');
 	}
 }
