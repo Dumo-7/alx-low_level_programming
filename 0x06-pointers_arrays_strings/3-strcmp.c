98259bc16a3bfd1b80921a13481e38b5f15150fa
Dumo-7 <deebozongwana@yahoo.com>
2023-07-28 15:35:00 -0700

    T3-5

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _strcmp - compares two lines of strings
+ * @s1: pointer to a character to be compared
+ * @s2: A pointer to a character that will be compared
+ *
+ * Return: difference between 2 pointers
+ */
+int _strcmp(char *s1, char *s2)
+{
+	char *str_a = s1;
+	char *str_b = s2;
+
+	while (*str_a != '\0' && *str_b != '\0')
+		if (*str_a != *str_b)
+		{
+			return (*str_a - *str_b);
+		}
+		if (*str_a == *str_b)
+		{
+			str_a++;
+			str_b++;
+		}
+
+}
---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * reverse_array - reverse array of integers
+ * @a: array
+ * @n: number of elements of array
+ *
+ * Return: void
+ */
+
+void reverse_array(int *a, int n)
+{
+	int l;
+	int x;
+
+	for (l = 0; l < n--; l++)
+	{
+		x = a[l];
+		a[l] = a[n];
+		a[n] = x;
+	}
+}
