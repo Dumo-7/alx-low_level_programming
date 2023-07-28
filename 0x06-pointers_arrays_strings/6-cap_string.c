a0ab57510a25d0ea82458843b2c83dc7cbc0a2ce
Dumo-7 <deebozongwana@yahoo.com>
2023-07-28 16:27:10 -0700

    T0 Fix

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -6,38 +6,30 @@
  *
  * Return: A pointer to the changed string.
  */
-
 char *cap_string(char *str)
 {
 	int index = 0;
-	int cap_next = 1;
 
 	while (str[index])
 	{
-		char a = str[index];
+		while (!(str[index] >= 'a' && str[index] <= 'z'))
+			index++;
 
-		if ((a >= 'a' && a <= 'z') || (a >= 'A' && a <= 'Z'))
-		{
-			if (cap_next)
-			{
-				if (a >= 'a' && a <= 'z')
-				{
-					str[index] = a - ('a' - 'A');
-				}
-			cap_next = 0;
-			}
-			else
-			{
-				if (a >= 'A' && a <= 'Z')
-				{
-					str[index] = a + ('a' - 'A');
-				}
-			}
-		}
-		else
-		{
-			cap_next = 1;
-		}
+		if (str[index - 1] == ' ' ||
+		    str[index - 1] == '\t' ||
+		    str[index - 1] == '\n' ||
+		    str[index - 1] == ',' ||
+		    str[index - 1] == ';' ||
+		    str[index - 1] == '.' ||
+		    str[index - 1] == '!' ||
+		    str[index - 1] == '?' ||
+		    str[index - 1] == '"' ||
+		    str[index - 1] == '(' ||
+		    str[index - 1] == ')' ||
+		    str[index - 1] == '{' ||
+		    str[index - 1] == '}' ||
+		    index == 0)
+			str[index] -= 32;
 
 		index++;
 	}
