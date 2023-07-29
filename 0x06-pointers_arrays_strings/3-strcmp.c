d5e8fa32c31a1cc0831531c6d84bf708d5405862
Dumo-7 <deebozongwana@yahoo.com>
2023-07-28 17:44:42 -0700

    T3 Update 3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -14,11 +14,13 @@ int _strcmp(char *s1, char *s2)
 	char *str_b = s2;
 
 	while (*str_a != '\0' && *str_b != '\0' && *str_a == *str_b)
+	{
+		str_a++;
+		str_b++;
+	}
 		if (*str_a != *str_b)
 		{
 			return (*str_a - *str_b);
 		}
-	str_a++;
-	str_b++;
 	return (*str_a - *str_b);
 }
