690b5fbba8d37fd09c939dbf22c0d5db4f056d0f
Dumo-7 <deebozongwana@yahoo.com>
2023-08-08 20:37:45 -0700

    Tasks

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stddef.h>
+#include <stdlib.h>
+
+
+
+/**
+ * _strdup - string duplicator function
+ * @str: string to be duplicated
+ * Return: a string pointer
+ */
+char *_strdup(char *str)
+{
+	int a = 1, b = 0;
+	char *s;
+
+	if (str == NULL)
+		return (NULL);
+	while (str[a])
+		a++;
+	s = (char *) malloc(a * sizeof(char) + 1);
+	if (s == NULL)
+		return (NULL);
+	while (b < a)
+	{
+		s[b] = str[b];
+		b++;
+	}
+	s[b] = '\0';
+	return (s);
+	_putchar('\n');
+}
+
---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,44 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * **alloc_grid - creates a two dimensional array of ints
+ * @width: width of the matrix
+ * @height: height of the matrix
+ * Return: pointer to the created grid (Success)
+ */
+
+int **alloc_grid(int width, int height)
+{
+	int **array;
+	int x, j;
+
+	if (height <= 0 || width <= 0)
+		return (NULL);
+
+	array = (int **) malloc(sizeof(int *) * height);
+
+	if (array == NULL)
+		return (NULL);
+	for (x = 0; x < height; x++)
+	{
+		array[x] = (int *) malloc(sizeof(int) * width);
+		if (array[x] == NULL)
+		{
+			free(array);
+			for (j = 0; j <= x; j++)
+				free(array[j]);
+			return (NULL);
+		}
+	}
+
+	for (x = 0; x < height; x++)
+	{
+		for (j = 0; j < width; j++)
+		{
+			array[x][j] = 0;
+		}
+	}
+	return (array);
+	_putchar('\n');
+}
