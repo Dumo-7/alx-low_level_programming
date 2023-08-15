98e8f2a17c7730b3ffb7ea826e629e52c4ee5860
Dumo-7 <deebozongwana@yahoo.com>
2023-08-15 16:38:02 -0700

    T2 Update

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -19,15 +19,6 @@ void print_dog(struct dog *d)
 		{
 			printf("Name: (nil)\n");
 		}
-		printf("Age: %.6f\n", d->age);
-		if (d->owner != NULL)
-		{
-			printf("Owner: %s\n", d->owner);
-		}
-		else
-		{
-			printf("Owner: (nil)\n");
-		}
 	}
 	printf("\n");
 }
